2471d39d502de1ee6b739133ab6f051e907bc62b
alexander blancheteau <alexblanch22@gmail.com>
2024-03-26 01:40:31 -0300

    Agrega .env

--- src/firebase/firebaseConfig.jsx
+++ src/firebase/firebaseConfig.jsx
@@ -1,21 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+
 // Configuración de tu aplicación Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyDDqtjZD4wSqIjT475Gr4TEvDVEAvPkY9o",
-  authDomain: "gloz-839ca.firebaseapp.com",
-  projectId: "gloz-839ca",
-  storageBucket: "gloz-839ca.appspot.com",
-  messagingSenderId: "744928909028",
-  appId: "1:744928909028:web:ff79240fc2719c8d877ef1"
-
-  //  apiKey: process.env.REACT_APP_apiKey,
-//  authDomain: process.env.REACT_APP_authDomain,
-//  projectId: process.env.REACT_APP_projectId,
-//  storageBucket: process.env.REACT_APP_storageBucket,
-//  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-//  appId: process.env.REACT_APP_appId
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Inicialización de Firebase
